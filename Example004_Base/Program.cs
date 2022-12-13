d24d5e7c2622e08edf1ed93dcb9a5280c3f1d68e
Anton <kondalov92@gmail.com>
2022-12-13 21:42:15 +0400

    Base

---
+++ Example004_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 2;
+int s = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if(a > max) max = a;
+if (b > max) max = b;
+if (s > max) max = s;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
